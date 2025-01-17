a7b235172773c60b8934e0ed79053405fbf3ffd3
IvanPopov8a <ivanpopov20101208@gmail.com>
2025-01-17 16:27:29 +0200

    UCHENIK

---
+++ char broi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int up = 0, low = 0, dig = 0;
+    char ch;
+
+    printf("Enter a bunch of symbols: ");
+    while (scanf("%c", &ch) && ch != '\n') {
+        if (ch >= 'A' && ch <= 'Z') up++;
+        else if (ch >= 'a' && ch <= 'z') low++;
+        else if (ch >= '0' && ch <= '9') dig++;
+    }
+
+    printf("Capital letters: %d\nSmall letters: %d\nDigits: %d\n", up, low, dig);
+printf("TI SI UCHENIK");
+    return 0;
+}
